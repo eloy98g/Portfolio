c6ed750e43768f653c7a9aeed1363d5b58149815
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-03-09 17:59:48 +0100

    first commit

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  primary: "#2CA3D0",
+  darkPrimary: "#196884",
+  secondary: "#2BA89F",
+  darkSecondary: "#197066",
+  accent: "#E1F533",
+  darkAccent: "#C2D327",
+  black: "black",
+  grey: "#9D9D9D",
+  lightenGrey: "#D8D7D5",
+  white: "white",
+  red: "#D53E58",
+  brown: "#AB6251",
+};
+
+export default colors;
