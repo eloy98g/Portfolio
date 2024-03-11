bf9c04228a4036b8d9b9d6f6736a17b8f2f4925e
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-03-11 10:47:52 +0100

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,29 @@
 import React from "react";
+import StyleSheet from "react-native-media-query";
 import { ExternalLink } from "lucide-react-native";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 // Components
 import TextButton from "./common/TextButton";
 import colors from "../theme/colors";
+import { useWindowDimensions } from "react-native";
+import { PHONE } from "../theme/breakPoints";
+import Divider from "./common/Divider";
 
 const Footer = () => {
+  const width = useWindowDimensions().width;
   const openSourceHandler = () => {};
   const reactNativeHandler = () => {};
   return (
-    <View style={styles.container}>
+    <View style={styles.container} dataSet={{ media: ids.container }}>
       <TextButton
         text="This page is open source!"
-        icon={<ExternalLink size={22} color={colors.grey} />}
+        icon={<ExternalLink size={20} color={colors.grey} />}
         onPress={openSourceHandler}
       />
+      {width < PHONE && <Divider height={12} />}
       <TextButton
-        text="💙 Made with React Native"
+        text="💙  Made with React Native"
         onPress={reactNativeHandler}
       />
     </View>
@@ -26,12 +32,17 @@ const Footer = () => {
 
 export default Footer;
 
-const styles = StyleSheet.create({
+const { ids, styles } = StyleSheet.create({
   container: {
     width: "100%",
     paddingVertical: 44,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    "@media (max-width: 768px)": {
+      flexDirection: "column",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
   },
 });
